1566f296c8ad57fce4a71493cc6129b7bba0ba2a
Catherine Miguel <catmig1992@gmail.com>
2022-09-27 12:36:17 -0700

    added directions, need to try bonus

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function newImage(url, left, bottom) {
   return object;
 }
 
+//When we make an item, we will give it the behavior to respond to being picked up.
+//Add an event listener to item that responds to the click event.
+//Since item is in scope, we can reference it within the event listener and use its remove method
+//Now that we can pick up items, let's add them to our inventory. Here's what we need to do:
+//Create an img element.
+//Define its src attribute using the URL parameter passed from above.
+//Append the img to inventory.
 function newItem(url, left, bottom) {
   let item = newImage(url, left, bottom);
   item.addEventListener("click", function () {
@@ -18,6 +25,9 @@ function newItem(url, left, bottom) {
   });
 }
 
+//To fix the scope bug, move the definition of inventory outside of newInventory.
+//This will make the inventory div a global variable, allowing us to reference and append to it within our event listener for picking up items.
+//At this point, you should be able to click on an item and see it move from the main area of the screen to inside your inventory.
 let inventory;
 function newInventory() {
   inventory = document.createElement("div");
@@ -46,3 +56,7 @@ newImage("assets/well.png", 500, 575);
 newItem("assets/sword.png", 500, 555);
 newItem("assets/shield.png", 165, 335);
 newItem("assets/staff.png", 600, 250);
+
+//Bonus
+//It doesn't make sense that newInventory() doesn't actually make our inventory div anymore. Try to move let inventory = document.createElement('div') back inside the newInventory function but define inventory as a global variable using return values.
+//Refactor the code so the logic for adding an item to the inventory is in its own function.
